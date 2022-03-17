11c2a9ab1027590fa5a0f1309008855efb72bb08
Matias Majul <majumatias@gmail.com>
2022-03-17 11:33:51 -0300

    update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,14 +16,10 @@ export const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">Link</a>
@@ -41,18 +37,13 @@ export const NavBar = () => {
             </li>
 
           </ul>
-          <FontAwesomeIcon icon={faFire} className='menu' />
+          <FontAwesomeIcon icon={faFire} className='' />
           <div>
             {
               isLogin ?
-                <div onClick={() => setVisible(!visible)} >
+                <div  >
                   <p>Bienvenido, !</p>
-                  <div
-                    className={`contentList ${visible ? 'show' : null}`}
-                    onMouseLeave={() => setVisible(!visible)}
-                  >
-                    <Link to={'/'} onClick={handleCloseSession}>Cerrar sesion</Link>
-                  </div>
+                    <Link to={'/'} onClick={handleCloseSession}>Cerrar sesion</Link>  
                 </div>
                 :
                 <div>
