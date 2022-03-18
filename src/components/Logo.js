3c7f14a32b7d11b33e27afcbbb8f456944d74840
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-17 22:51:12 -0300

    Fix merge eslint

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,23 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRocket } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRocket } from '@fortawesome/free-solid-svg-icons';
 
-import '../styles/Logo.css'
+import '../styles/Logo.css';
 
 const Logo = () => {
 
-    return (
-        <div className="col-4 d-flex justify-content-center align-items-center p-2">
-            <Link to="/homeRegistered" className="d-flex flex-row gap-2 text-decoration-none">
-                <FontAwesomeIcon icon={faRocket} className="color-light size-icon" />
-                <div className="d-flex flex-column align-items-center fw-bold">
-                    <span className="color-light " >HACKATECH #4</span>
-                    <span className="fs-6 text-primary-50 ">Eventos TECO</span>
-                </div>
-            </Link>
-        </div>
-    );
-}
+	return (
+		<div className="col-4 d-flex justify-content-center align-items-center p-2">
+			<Link to="/homeRegistered" className="d-flex flex-row gap-2 text-decoration-none">
+				<FontAwesomeIcon icon={faRocket} className="color-light size-icon" />
+				<div className="d-flex flex-column align-items-center fw-bold">
+					<span className="color-light " >HACKATECH #4</span>
+					<span className="fs-6 text-primary-50 ">Eventos TECO</span>
+				</div>
+			</Link>
+		</div>
+	);
+};
 
 export default Logo;  
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,19 @@ import { Link } from 'react-router-dom';
 import { userContext } from '../context/UserContext';
 
 const NavBar = () => {
-  const { isLogin } = useContext(userContext);
+	const { isLogin } = useContext(userContext);
 
-
-  return (
-    <div className="col-4">
-      {
-        isLogin() ?
-          <nav className="navbar navbar-expand-md navbar-light bg-light">
-            <div className="container-fluid">
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item dropdown">
-                    <Link className="nav-link dropdown-toggle text-dark" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Eventos
+	return (
+		<div className="col-4">
+			{
+				isLogin() ?
+					<nav className="navbar navbar-expand-md navbar-light bg-light">
+						<div className="container-fluid">
+							<div className="collapse navbar-collapse" id="navbarSupportedContent">
+								<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+									<li className="nav-item dropdown">
+										<Link className="nav-link dropdown-toggle text-dark" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Eventos
 										</Link>
 										<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
 											<li><Link className="dropdown-item" to="/nextevents">Próximos</Link></li>
