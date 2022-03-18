014630b7603a7661658ba57b1813e1408131cbd9
Joaquin (Pato) Decima <joaquin.decima@gmail.com>
2022-03-18 00:07:58 -0300

    Fix Footer.js use bootstrap remove unused files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,53 @@
 import React from 'react';
-import { Box, Container, Row, Column, FooterLink, Heading, } from './FooterStyles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faTwitter, faInstagram} from '@fortawesome/fontawesome-free-brands';
+
+
 
 const Footer = () => {
 	return (
-		<Box>
-			<Container>
-				<Row>
-					<Column>
-						<Heading>ACERCA DE</Heading>
-						<FooterLink href="#">Compañia</FooterLink>
-						<FooterLink href="#">Equipo</FooterLink>
-						<FooterLink href="#">Inversores</FooterLink>
-					</Column>
-					<Column>
-						<Heading>ASISTENCIA</Heading>
-						<FooterLink href="#">Somos Telecom</FooterLink>
-						<FooterLink href="#">Enacom contacto@enacom.gob.ar</FooterLink>
-						<FooterLink href="#">Oferta interconexión</FooterLink>
-						<FooterLink href="#">Operadores moviles</FooterLink>
-					</Column>
-					<Column>
-						<Heading>CONFIANZA EN TUS COMPRAS</Heading>
-						<FooterLink href="#">Política de Privacidad de Telecom</FooterLink>
-						<FooterLink href="#">Política de Uso Aceptable de Servicios de Telecom</FooterLink>
-					</Column>
-					<Column>
-						<Heading>AYUDA</Heading>
-						<FooterLink href="#">Contacto</FooterLink>
-						<FooterLink href="#">Centro de Ayuda</FooterLink>
-						<FooterLink href="#">Telefonía fija hogar</FooterLink>
-						<FooterLink href="#">
-							<i className="fab fa-instagram">
-								<span style={{ marginLeft: '10px' }}>
-				Instagram
-								</span>
-							</i>
-						</FooterLink>
-						<FooterLink href="#">
-							<i className="fab fa-twitter">
-								<span style={{ marginLeft: '10px' }}>
-				Twitter
-								</span>
-							</i>
-						</FooterLink>
-					</Column>
-				</Row>
-			</Container>
-		</Box>
+		<footer className="bg-dark pt-4 pb-4" id="footer">
+			<div className="container">
+				<div className="row text-center">
+					<div className="col-md-3">
+						<h3 className="text-white">ACERCA DE</h3>
+						<ul className="list-group mt-3">
+							<li className="list-group-item bg-dark text-white">Compañia</li>
+							<li className="list-group-item bg-dark text-white">Equipo</li>
+							<li className="list-group-item bg-dark text-white">Inversores</li>
+						</ul>
+					</div>
+					<div className="col-md-3">
+						<h3 className="text-white">ASISTENCIA</h3>
+						<ul className="list-group mt-3">
+							<li className="list-group-item bg-dark text-white">Somos Telecom</li>
+							<li className="list-group-item bg-dark text-white">Enacom contacto@enacom.gob.ar</li>
+							<li className="list-group-item bg-dark text-white">Oferta interconexión</li>
+							<li className="list-group-item bg-dark text-white">Operadores moviles</li>
+						</ul>
+					</div>
+					<div className="col-md-3">
+						<h4 className="text-white">CONFIANZA EN TUS COMPRAS</h4>
+						<ul className="list-group mt-3">
+							<li className="list-group-item bg-dark text-white">Política de Privacidad de Telecom</li>
+							<li className="list-group-item bg-dark text-white">Política de Uso Aceptable de Servicios de Telecom</li>
+						</ul>
+					</div>
+					<div className="col-md-3">
+						<h3 className="text-white">AYUDA</h3>
+						<ul className="list-group mt-3">
+							<li className="list-group-item bg-dark text-white">Contacto</li>
+							<li className="list-group-item bg-dark text-white">Centro de Ayuda</li>
+							<li className="list-group-item bg-dark text-white">Telefonía fija hogar</li>
+						</ul>
+					</div>
+					<div className="col-12 text-center mt-3">
+						<a href="#" className="ms-1 me-1 text-white"><FontAwesomeIcon icon={faInstagram}/></a>
+						<a href="#" className="ms-1 me-1 text-white"><FontAwesomeIcon icon={faTwitter}/></a>
+					</div>
+				</div>
+			</div>
+		</footer>
 	);
 };
 export default Footer;
